880dca27518cef1a3e4aa0b0a753083a8b0d69da
pacogit <elmor1@hotmail.com>
2023-11-10 02:11:55 +0100

    added GUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import React, { useState } from 'react';
+
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log(inputValue);
+  };
+
+  return (
+    <div>
+      <h1>Application</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleButtonClick}>Submit</button>
+    </div>
+  );
 }
 
-export default App;
+export default App; 
