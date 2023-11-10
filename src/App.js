295ef19637014344a40dd28ed5706f811408084e
pacogit <elmor1@hotmail.com>
2023-11-10 02:23:02 +0100

    Hola Mundo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import React, { useState } from 'react';
 
-
-
 function App() {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-    console.log(inputValue);
-  };
-
-  return (
-    <div>
-      <h1>Application</h1>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <button onClick={handleButtonClick}>Submit</button>
-    </div>
-  );
+  print("Hola Mundo")
 }
 
 export default App; 
